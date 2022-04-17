15900cc2ea1f85d2cbd2f04e544b33dd28a608e9
Anna Filippova <anna.filippova.8@yandex.ru>
2022-04-17 23:26:21 +0300

    setted up page object, added component and some random strings

--- src/test/java/filippova/anna/PracticeFormTests.java
+++ src/test/java/filippova/anna/PracticeFormTests.java
@@ -2,8 +2,12 @@ package filippova.anna;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.github.javafaker.Faker;
+import filippova.anna.pages.RegistrationFormPage;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.Locale;
 
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
@@ -11,63 +15,59 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class PracticeFormTests {
 
+    Faker fakerEn = new Faker(new Locale("en"));
+    Faker fakerRu = new Faker(new Locale("ru"));
+    RegistrationFormPage registrationFormPage = new RegistrationFormPage();
+
+    String firstName = fakerRu.name().firstName(),
+            lastName = fakerRu.name().lastName(),
+            email = fakerEn.internet().emailAddress(),
+            gender = "Female",
+            userNumber = fakerRu.phoneNumber().subscriberNumber(10),
+            keyForSubjects = "a",
+            subjects = "Arts",
+            hobbies = "Music",
+            picture = "picture.jpg",
+            currentAddress = fakerRu.address().fullAddress(),
+            state = "Haryana",
+            city = "Karnal",
+            title = "Thanks for submitting the form";
+
     @BeforeAll
     static void setUp() {
-        //Configuration.holdBrowserOpen = true;
+        Configuration.holdBrowserOpen = true;
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
     }
 
     @Test
     void fillFormTest() {
-        String firstName = "Anna";
-        String lastName = "Filippova";
-        String email = "anna@filippova.com";
-        String gender = "Female";
-        String mobileNumber = "9998887766";
-        String subjects = "Arts";
-        String hobbies = "Music";
-        String picture = "picture.jpg";
-        String currentAddress = "Some street 1";
-        String state = "Haryana";
-        String city = "Karnal";
+        registrationFormPage.openPage()
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setEmail(email)
+                .setGender(gender)
+                .setUserNumber(userNumber)
+                .setBirthDate("22", "April", "1998")
+                .setSubjects(keyForSubjects, subjects)
+                .setHobbies(hobbies)
+                .uploadPicture(picture)
+                .setCurrentAddress(currentAddress)
+                .setState(state)
+                .setCity(city)
+                .clickSubmit()
+                .checkResultTitle(title)
+                .checkResultStudentName("Student Name", firstName, lastName)
+                .checkResultStudentEmail("Email", email)
+                .checkResultGender("Gender", gender)
+                .checkResultMobile("Mobile", userNumber)
+                .checkResultDateOfBirth("Date of Birth", "22 April,1998")
+                .checkResultSubjects("Subjects", subjects)
+                .checkResultHobbies("Hobbies", hobbies)
+                .checkResultPicture("Picture", picture)
+                .checkResultAddress("Address", currentAddress)
+                .checkResultStateAndCity("State and City", state, city)
+                .closeResult();
 
-        open("/automation-practice-form");
-
-        $("[id=firstName]").setValue(firstName);
-        $("[id=lastName]").setValue(lastName);
-        $("[id=userEmail]").setValue(email);
-        $("[id=genterWrapper]").$(byText(gender)).click();
-        $("[id=userNumber]").setValue(mobileNumber);
-        $("[id=dateOfBirthInput]").click();
-        $(".react-datepicker__month-select").selectOption("April");
-        $(".react-datepicker__year-select").selectOption("1998");
-        $("[aria-label='Choose Wednesday, April 22nd, 1998']").click();
-        $("#subjectsInput").sendKeys("a");
-        $("#subjectsWrapper").$(byText(subjects)).click();
-        $("[id=hobbiesWrapper]").$(byText(hobbies)).click();
-        $("[id=uploadPicture]").uploadFromClasspath(picture);
-        $("[id=currentAddress]").setValue(currentAddress);
-        $("[id=state]").click();
-        $("[id=stateCity-wrapper]").$(byText(state)).click();
-        $("[id=city]").click();
-        $("[id=stateCity-wrapper]").$(byText(city)).click();
-        $("[id=submit]").click();
-
-        //Asserts
-        $("[id=example-modal-sizes-title-lg]").shouldHave(Condition.text("Thanks for submitting the form"));
-        $(".table-responsive").shouldHave(Condition.text(firstName + " " + lastName),
-                Condition.text(email),
-                Condition.text(gender),
-                Condition.text(mobileNumber),
-                Condition.text("22 April,1998"),
-                Condition.text(subjects),
-                Condition.text(hobbies),
-                Condition.text(picture),
-                Condition.text(currentAddress),
-                Condition.text(state + " " + city)
-        );
-        $("[id=closeLargeModal]").click();
     }
-
 }
